0d07826c7e3cac603119db0bcf68f049ba0927d3
Jason Dean <deanjm@gmail.com>
2017-12-09 17:53:13 -0500

    Setting up the website and webapi controller

---
+++ RecipeCard/Controllers/RecipesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeCard.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class RecipesController : Controller
+    {
+        [HttpPost]
+        [ActionName("Read")]
+        public IActionResult Read(string url)
+        {
+            return Json("Not implemented");
+        }
+    }
+}
